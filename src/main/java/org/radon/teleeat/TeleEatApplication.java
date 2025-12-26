3246fae26939b0448eeb03080bb895cc1f748dfb
ar.kherazan <a.kherazan@nizek.com>
2025-12-27 00:28:50 +0330

    init : Project initialize commit

---
+++ src/main/java/org/radon/teleeat/TeleEatApplication.java
@@ -0,0 +1,13 @@
+package org.radon.teleeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeleEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/radon/teleeat/TeleEatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.radon.teleeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
